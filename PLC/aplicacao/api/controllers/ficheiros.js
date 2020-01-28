2e687f40bb34fb9c6f0e7e27476f6cba703de24d
João Pimentel <32567686+citoplasme@users.noreply.github.com>
2020-01-28 18:50:39 +0000

    Update ficheiros.js

--- PLC/aplicacao/api/controllers/ficheiros.js
+++ PLC/aplicacao/api/controllers/ficheiros.js
@@ -2,18 +2,21 @@ var Ficheiro = require('../models/ficheiros')
 module.exports.listar = () => {
     return Ficheiro
         .find()
+        .sort({data : -1})
         .exec()
 }
 
 module.exports.listar_uc = (u) => {
     return Ficheiro
         .find({uc : u})
+        .sort({data : -1})
         .exec()
 }
 
 module.exports.listar_uc_tipo = (u, t) => {
     return Ficheiro
         .find({uc : u, tipo : t})
+        .sort({data : -1})
         .exec()
 }
 
